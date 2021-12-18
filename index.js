9f8c4f23abe031b38120fedba728ef81035ec23f
Michael Jordan <mjthecoder53161@gmail.com>
2021-12-18 14:28:19 +0900

    Connected the app to Mongo Database

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-
+const mongoose = require('mongoose');
 const config = require('config');
 const genres = require('./routes/genres');
 const express = require('express');
@@ -7,6 +7,10 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended : false }));
 app.use('/api/genres', genres);
+
+mongoose.connect(config.get("mongoURI"))
+.then(() => console.log("Connected to mongoDB..."))
+.catch(err => console.log('FATAL ERROR : Failed to connect to mongoDB', err));
 
 app.get('/', (req, res) => { 
     res.send("Welcome to vibes");
