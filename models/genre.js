f1a7ebf5ff44f8549519d2d7a8fb8cd3a3fbc206
Michael Jordan <mjthecoder53161@gmail.com>
2021-12-18 14:36:14 +0900

    Created model for music genre

---
+++ models/genre.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const Joi = require('joi');
+
+const genreSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        minLength: 3,
+        maxLength: 30
+    }
+});
+
+const Genre = mongoose.model('Genre', genreSchema);
+
+
+function validateGenre(genre) {
+    const schema = Joi.object({
+        name: Joi.string().min(3).max(30).required()
+    });
+
+    return schema.validate(genre);
+}
+
+module.exports = { validateGenre, Genre };
+
+
