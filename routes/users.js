957483f9ed0fd4fdcb1104202951fd046b9790b6
Michael Jordan <mjthecoder53161@gmail.com>
2021-12-22 11:04:55 +0900

    Created a router for registering user

---
+++ routes/users.js
@@ -0,0 +1,18 @@
+const { User, validate } = require('../models/user');
+const router = require('express').Router();
+
+router.post('/', async (req, res) => {
+    const { error } = validate(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+
+    let user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+    });
+
+    user = await user.save();
+    res.send(user);
+
+});
