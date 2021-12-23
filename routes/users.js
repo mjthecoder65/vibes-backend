c37ae71ea260b3e7b1f23a48994e46e083ac1a23
Michael Jordan <mjthecoder53161@gmail.com>
2021-12-24 01:35:53 +0900

    Refactored user routes

--- routes/users.js
+++ routes/users.js
@@ -1,29 +1,30 @@
-const { User, validate } = require('../models/user');
-const router = require('express').Router();
-const _ = require('lodash');
-const bcrypt = require('bcrypt');
-const auth = require('../middleware/auth');
+const { User, validate } = require("../models/user");
+const router = require("express").Router();
+const _ = require("lodash");
+const bcrypt = require("bcrypt");
+const auth = require("../middleware/auth");
 
-router.post('/', async (req, res) => {
-    const { error } = validate(req.body);
-    if (error) return res.status(400).send(error.details[0].message);
+router.post("/", async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
 
-    let user = await User.findOne({ email: req.body.email });
-    if (user) return res.status(400).send("User Already Registered");
+  let user = await User.findOne({ email: req.body.email });
+  if (user) return res.status(400).send("User Already Registered");
 
-    user = new User(_.pick(req.body, ['name', 'email', 'password']));
-    const salt = await bcrypt.genSalt(10);
-    user.password = await bcrypt.hash(user.password, salt);
+  user = new User(_.pick(req.body, ["name", "email", "password"]));
+  const salt = await bcrypt.genSalt(10);
+  user.password = await bcrypt.hash(user.password, salt);
 
-    await user.save();
-    const token = user.generateAuthToken();
+  await user.save();
+  const token = user.generateAuthToken();
 
-    res.header('x-auth-token', token).send(_.pick(user, ['_id', 'name', 'email']));
+  res.header("x-auth-token", token)
+    .send(_.pick(user, ["_id", "name", "email"]));
 });
 
-router.get('/me', auth, async (req, res) => {
-    const user = await User.findById(req.user._id).select('-password');
-    res.send(user);
+router.get("/me", auth, async (req, res) => {
+  const user = await User.findById(req.user._id).select("-password");
+  res.send(user);
 });
 
 module.exports = router;
